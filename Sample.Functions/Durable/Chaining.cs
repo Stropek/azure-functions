695a2225ec892c04e2cf9d7a0ea6bc7f7a825524
Przemysław Curzytek <curzytekp@gmail.com>
2019-09-20 01:47:13 -0400

    remove blob, add durable, update slides

---
+++ Sample.Functions/Durable/Chaining.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Sample.Functions.Durable
+{
+    public static class Chaining
+    {
+        [FunctionName("Chaining")]
+        public static async Task<List<string>> RunOrchestrator(
+            [OrchestrationTrigger] DurableOrchestrationContext context)
+        {
+            var outputs = new List<string>();
+
+            // Replace "hello" with the name of your Durable Activity Function.
+            outputs.Add(await context.CallActivityAsync<string>("Chaining_Hello", "Iron Man"));
+            outputs.Add(await context.CallActivityAsync<string>("Chaining_HowAreYou", "Iron Man"));
+            outputs.Add(await context.CallActivityAsync<string>("Chaining_Goodbye", "Iron Man"));
+
+            return outputs;
+        }
+
+        [FunctionName("Chaining_Hello")]
+        public static string SayHello([ActivityTrigger] string name, ILogger log)
+        {
+            log.LogInformation($"Saying hello to {name}.");
+            return $"Hello {name}!";
+        }
+
+        [FunctionName("Chaining_Goodbye")]
+        public static string SayGoodbye([ActivityTrigger] string name, ILogger log)
+        {
+            log.LogInformation($"Saying goodbye to {name}.");
+            return $"Goodbye {name}!";
+        }
+
+        [FunctionName("Chaining_HowAreYou")]
+        public static string SayHowAreYou([ActivityTrigger] string name, ILogger log)
+        {
+            log.LogInformation($"Saying how are you to {name}.");
+            return $"How are you, {name}?";
+        }
+
+        [FunctionName("DurableIronMan")]
+        public static async Task<HttpResponseMessage> HttpStart(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")]HttpRequestMessage req,
+            [OrchestrationClient]DurableOrchestrationClient starter,
+            ILogger log)
+        {
+            // Function input comes from the request content.
+            string instanceId = await starter.StartNewAsync("Chaining", null);
+
+            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+
+            return starter.CreateCheckStatusResponse(req, instanceId);
+        }
+    }
+}
