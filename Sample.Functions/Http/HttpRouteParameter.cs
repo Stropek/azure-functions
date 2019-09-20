6f328e8b62306b46aae1cf057d650ca874eb194c
Przemysław Curzytek <curzytekp@gmail.com>
2019-09-20 00:05:13 -0400

    add proxies and other http requests

---
+++ Sample.Functions/Http/HttpRouteParameter.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Sample.Functions.Http
+{
+    public static class HttpRouteParameter
+    {
+        [FunctionName("HttpRouteParameter")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "/parameter/{id}")] HttpRequest req,
+            string id,
+            ILogger log)
+        {
+            var message = $"Just passing a parameter in route => id: {id}";
+
+            log.LogInformation(message);
+
+            return new OkObjectResult(message);
+        }
+    }
+}
---
+++ Sample.Functions/Http/HttpWithObjectInJson.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Sample.Functions.Http
+{
+    public class Superhero
+    {
+        public string Name { get; set; }
+
+        public string Team { get; set; }
+    }
+
+    public static class HttpParameter
+    {
+        [FunctionName("HttpParameter")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "/parameter/{id}")] Superhero superhero,
+            ILogger log)
+        {
+            var message = $"Superhero name: {superhero.Name} [team: {superhero.Team}]";
+
+            log.LogInformation(message);
+
+            return new OkObjectResult(message);
+        }
+    }
+}
